dcfa345167768a2cd0729361548ab57499618669
Gabriele Toselli <toselli.gabriele@gmail.com>
2025-06-06 14:17:47 +0200

    wip

--- packages/ui-shadcn/app/page.tsx
+++ packages/ui-shadcn/app/page.tsx
@@ -1,6 +1,8 @@
 import { getJobs, getQueues } from "@/lib/db";
 import Dashboard from "./dashboard";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const jobs = await getJobs({});
   const queues = await getQueues();
