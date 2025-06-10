7539ff5c00b02be424797d6c0ad1c3e3f7c33cfe
Gabriele Toselli <toselli.gabriele@gmail.com>
2025-06-10 09:09:35 +0200

    chore: add cspell with eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,30 @@
 // @ts-check
 
+import cspellESLintPluginRecommended from "@cspell/eslint-plugin/recommended";
 import eslint from "@eslint/js";
 import { globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, globalIgnores(["**/dist/**"]), {
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-      },
-    ],
-    "func-style": ["error", "declaration"],
-    "object-shorthand": ["error"],
-    "sort-imports": ["error", { ignoreCase: true }],
-    "sort-keys": ["error", "asc", { caseSensitive: true, natural: true }],
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  cspellESLintPluginRecommended,
+  globalIgnores(["**/dist/**"]),
+  {
+    rules: {
+      "@cspell/spellchecker": ["error", {}],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "func-style": ["error", "declaration"],
+      "object-shorthand": ["error"],
+      "sort-imports": ["error", { ignoreCase: true }],
+      "sort-keys": ["error", "asc", { caseSensitive: true, natural: true }],
+    },
   },
-});
+);
