dfa39d810ad28ec46344a6ea4cdfa009c0f8c389
Gabriele Toselli <toselli.gabriele@gmail.com>
2025-10-18 13:47:40 +0200

    feat(core): add ability to include periodic schedule time in payload

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -69,7 +69,7 @@ export interface RetrieveQueueParams {
 
 export interface AddParams {
   name: string;
-  payload: unknown;
+  payload: Record<string, unknown>;
   priority?: number;
   startAfter?: Date;
   idempotentKey?: string;
@@ -102,6 +102,7 @@ export interface PeriodicJob {
   jobTemplate: Omit<AddParams, "idempotentKey">;
   catchUpStrategy: "all" | "latest" | "none";
   maxCatchUp?: number; // default: 100, only applies to 'all' strategy
+  includeScheduledTime?: boolean; // default: false, adds _periodic.scheduledTime to payload
 }
 
 export type WorkOptions = {
